fcdb5bed41b9a3a927b0854ae344f30a516cea3e
rebecca_alvarez <realvarez.dwfc5@ucamp.utel.edu.mx>
2022-04-08 21:30:29 -0500

    Se agregaron las funciones crear y read, dentro de las cuales podemos leer e imprimir en pantalla (no solo en consola) los valores que se ingresen con los promps. ADemas comentamos y limpiamos el codigo

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ let arr = [
 // Definimos un arreglo vacio
 let datos=[]
 // Agregamos un listener al boton agregar
-agregar.addEventListener('click', () => {
+agregar.addEventListener('click', () => 
+{
 // limpiamos el arreglo para que no guarde acumule las repuestas
     datos=[]
 // Ciclo para pedir datos en prompt
@@ -20,21 +21,35 @@ agregar.addEventListener('click', () => {
 
     }
     // alert(datos);
-// Llamamos a la funcion render (aun sin datos)
-renderElement ()
-})
+    
+    // Creamos diccionario para almacenar datos
+    let diccionario={
+        fecha:datos[0],
+        tipo:datos[1],
+        descripcion:datos[2],
+        contacto:datos[3],
+        costo:datos[4]
+    }
+    let keys = Object.keys(localStorage);
+    let contador = keys.length;
+    // alert(contador)
 
-function renderElement() {
+    let contadorPedidos=contador+1;
+    crear(numeroPedido=contadorPedidos,datosPedido=diccionario);
+    read();
+})
 // Definimos variable tabla para llamar de html la sección filas
-    let table = document.getElementById('filas');
+var table = document.getElementById('filas');
+function renderElement() {
+
     // table.innerHTML = 'Aquó estoy'
 // Definimos variable para guardar info de filas
     let codehtml = '<tr>';
     // table.insertRow(0)
 // Cada dato lo asignamos en un td
-    datos.forEach(function (el) {
-      codehtml += `<td>${el}</td>`;
-    })
+    for (el in datos) {
+      codehtml += `<td>${datos[el]}</td>`;
+    }
 // Concatenamos los datos con los botones borrar y editar
     codehtml += `<td>
     <div class="btn-group me-2" role="group" aria-label="Second group">
@@ -44,6 +59,33 @@ function renderElement() {
     </td>`
     codehtml += '</tr>'
 // agregarmos a la tabla los datos de code con los botones
-    table.innerHTML = codehtml
-
+    table.innerHTML += codehtml
   }
+function crear (numeroPedido,datosPedido){
+    // Buscamos el item pedido1 en la variable pedido1 que antes llenamos con el diccionario y lo guardamos en localstorage
+    localStorage.setItem(numeroPedido, JSON.stringify(datosPedido))
+}
+// Creamos función read
+function read (){
+    // Definimos variable vacia
+    var values = []
+    // definimos variable keys y revisamos el diccionario en el localstorage
+    let keys = Object.keys(localStorage)
+    // Contamos las veces que se repite la variable keys en el localstrorage
+    let contador = keys.length;
+    // En el HTML limpiamos el registro de table que direcciona el tbody(tabla)
+    table.innerHTML="";
+    // Hacemos un ciclo para poder encontrar el numero de registo ingresado y ademas hacemos un llamado a la funcion renderElement(la que se encarga de imprimir)
+    for(i in keys){
+
+        // values.push(localStorage.getItem(keys[i]));
+        // la variable datos se transforma en lo que contenga el local Storage, pero el parse convierte de nuevo en diccionario un string
+        datos=JSON.parse(localStorage.getItem(keys[i]));
+        // Invocamos a la funcion render
+        renderElement();
+    }
+    
+    // datos=values;
+    // renderElement();
+}
+
